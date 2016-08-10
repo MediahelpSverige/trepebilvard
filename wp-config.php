0a8deb008f9b2be2842fefda15454242d6ecda31
alienwizard <johanwendin@gmail.com>
2016-08-10 14:26:02 +0200

    config fixesv2

--- wp-config.php
+++ wp-config.php
@@ -20,13 +20,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'database_name_here');
+define('DB_NAME', 'trepe');
 
 /** MySQL database username */
-define('DB_USER', 'username_here');
+define('DB_USER', 'cb30357e8491');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'password_here');
+define('DB_PASSWORD', 'steel1992');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
